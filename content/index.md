c2cfed418c7f0627b585c6b01895533b4cb07273
Flavien Bourras <flavien.bourras@gmail.com>
2024-01-13 17:49:08 +0100

    Quartz sync: Jan 13, 2024, 5:49 PM

---
+++ content/index.md
@@ -0,0 +1,17 @@
+---
+title: Maths
+draft: false
+tags: 
+  - Maths
+---
+
+## Terminale
+- [Sésmath 2020](https://manuel.sesamath.net/numerique/index.php?ouvrage=mstsspe_2020&page_gauche=2)
+## Première
+- [Sésamath 2019](https://manuel.sesamath.net/numerique/index.php?ouvrage=ms1spe_2019&page_gauche=2)
+## Seconde
+
+## Troisième
+## Quatrième
+## Cinquième
+## Sixième
--- quartz.config.ts
+++ quartz.config.ts
@@ -3,13 +3,13 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Flavien Bourras, CFA",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "flavinsh.github.io/notes",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
