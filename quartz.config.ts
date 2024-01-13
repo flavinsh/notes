6369a2a4e8030c36f09c7220263741acc627c960
Flavien Bourras <flavien.bourras@gmail.com>
2024-01-13 23:36:06 +0100

    Quartz sync: Jan 13, 2024, 11:36 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Flavien Bourras, CFA",
+    pageTitle: "📖 Flavien Bourras, CFA",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
